a310dab75efd5b83bd9d3152c03b132f8797f12a
Svetlana <zhavzharova2000@yandex.ru>
2022-09-15 00:07:41 +0300

    HTTP-server

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	todo "github.com/zhavzharik/REST-API-in-Go"
+	"github.com/zhavzharik/REST-API-in-Go/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
