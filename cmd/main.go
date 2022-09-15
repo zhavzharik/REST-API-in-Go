f2d726e40152be869cceac4797c1fd66204a4d20
Svetlana <zhavzharova2000@yandex.ru>
2022-09-15 20:46:34 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	todo "github.com/zhavzharik/REST-API-in-Go"
 	"github.com/zhavzharik/REST-API-in-Go/pkg/handler"
 	"github.com/zhavzharik/REST-API-in-Go/pkg/repository"
@@ -9,12 +10,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
-	
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
