5dc279d98cacdc31b09fa726251e7a090454c90c
Svetlana <zhavzharova2000@yandex.ru>
2022-09-16 16:30:45 +0300

    authentication, jwt-token

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -14,7 +14,7 @@ func (h *Handler) signUp(c *gin.Context) {
 		return
 	}
 
-	id, err := h.services.CreateUser(input)
+	id, err := h.services.Authorization.CreateUser(input)
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -24,6 +24,26 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (h *Handler) signIn(c *gin.Context) {
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": token,
+	})
 
 }
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users
 (
-    id          serial          not null unique,
-    name        varchar(255)    not null,
-    username    varchar(255)    not null unique
+    id              serial          not null unique,
+    name            varchar(255)    not null,
+    username        varchar(255)    not null unique,
+    password_hash   varchar(255)    not null
 );
 
 CREATE TABLE todo_lists
