b78792c3e65df1241ee0ec1296bd03c518ca6b0d
Svetlana <zhavzharova2000@yandex.ru>
2022-09-18 18:49:09 +0300

    add delete list, update list

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -9,6 +9,10 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+type StatusResponse struct {
+	Status string `json:"status"`
+}
+
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Error(message)
 	c.AbortWithStatusJSON(statusCode, ErrorResponse{message})
